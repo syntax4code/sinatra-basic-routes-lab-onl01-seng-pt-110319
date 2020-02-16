321f574523854668e9f9851c192db1459a469542
Warren Harper II <warrenharper2@att.net>
2020-02-16 20:13:13 +0000

    Automatically backed up by Learn

--- models/dog.rb
+++ models/dog.rb
@@ -1,8 +1,18 @@
 
 class Dog
-@name = name
-@breed = breed
-@age = age
+  attr_accessor :name, :breed, :age 
+  def name
+    @name = name
+  end
+  
+  def breed
+    @breed = breed
+  end
+  
+  def age
+    @age = age
+  end 
+  
 @@all = []
 
 end
