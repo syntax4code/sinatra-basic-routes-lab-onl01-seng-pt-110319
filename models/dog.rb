297a92db63df6e1c89028b029b7812cf638ace71
Warren Harper II <warrenharper2@att.net>
2020-02-16 20:13:44 +0000

    Done.

---
+++ models/dog.rb
@@ -0,0 +1,18 @@
+
+class Dog
+  attr_accessor :name, :breed, :age
+  def name
+    @name = name
+  end
+
+  def breed
+    @breed = breed
+  end
+
+  def age
+    @age = age
+  end
+
+@@all = []
+
+end
