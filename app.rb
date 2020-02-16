cc6b24b4a83454a14b74488c232edba84f3a5784
Warren Harper II <warrenharper2@att.net>
2020-02-16 18:50:57 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,17 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/name' do
+  "My name is Loy."
+end
+
+get '/hometown' do
+"My hometown is Fort Worth."
+end
+
+get '/favorite-song' do
+"My favorite song is 'chain breaker'."
+#erb :'song/index.html.erb'
+end
+
 end
